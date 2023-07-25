e0cab928fcfd0507d4c089cc2b5686eb384e21b3
sahitid <sahitiaws@gmail.com>
2023-07-25 01:05:15 -0400

    more edits!

--- components/head.js
+++ components/head.js
@@ -1,15 +1,13 @@
 import Head from 'next/head'
 
 export default function HeadObject({children}) {
-    const title = "The Perfect Next.js Theme";
-    const description = "This is the perfect Next.js theme that you should use instead of create-next-app.";
-    const searchBarColor = "#ffffff"; // This is your Safari 15 Search Bar Color in Light Mode
-    //const darkSearchBarColor = "#000000"; // This is your Safari 15 Search Bar Color in Dark Mode (optional)
-    const keywords = "put some fun keywords here";
-    const author = "Sarthak Mohanty";
-    const twitter = "@sarthaktexas";
-    const url = "http://localhost:3000"; // This is your og:url or domain (optional but recommended)
-    //const image = "/ogimage.png"; // This is your OpenGraph image
+    const title = "Sahiti's Personal Website";
+    const description = "my personal site <3";
+    const searchBarColor = "#ffffff";
+    const keywords = "sahiti, sahiti dasari";
+    const author = "Sahiti Dasari";
+    const twitter = "@null";
+    const url = "http://localhost:3000";
     return (
         <Head>
             <meta charSet="utf-8" />
